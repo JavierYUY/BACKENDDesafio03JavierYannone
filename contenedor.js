bf19a904786cb932e666c3a3340f353209e800d5
Javier Yannone <javieryannone@gmail.com>
2022-12-01 23:10:58 -0300

    subida de archivos

---
+++ contenedor.js
@@ -0,0 +1,130 @@
+const { info, Console } = require('console');
+const fs = require('fs');
+const { open, close } = require('node:fs');
+
+class Contenedor{
+    constructor(path){
+        this.path = path;
+    }
+
+    async save(objeto){
+        const objs = await this.getAll();
+
+        if (objs.length > 0) {
+            const objLast = objs[objs.length - 1];
+
+            const newObj = {
+                title : objeto.titles,
+                price : objeto.price,
+                thumbnail : objeto.thumbnail,
+                id : objLast.id + 1
+            }
+
+            objs.push(newObj);
+        }else{
+            const newObj = {
+                title : objeto.titles,
+                price : objeto.price,
+                thumbnail : objeto.thumbnail,
+                id : 1
+            }
+
+            objs.push(newObj);
+        }
+
+        await fs.promises.writeFile(this.path, JSON.stringify(objs, null, 2))
+            .then(() => console.log('Objeto guardado'))
+            .catch(error => console.log('Error en save' + error))
+    }
+
+    async getById(id){
+        const objs = await this.getAll();
+        const buscando = objs.find(ob => ob.id === id);
+        return buscando;
+    }
+
+    async getAll(){
+        
+        //let objs = [];
+        try{
+            console.log('estoy leyendo')
+            const objs = await fs.readFile("productos.txt", 'utf-8');
+            console.log('dentrooooo');
+            return JSON.parse(objs);
+            
+            /*
+            return new Promise(res =>{
+                setTimeout(() =>{
+                    const objs = fs.readFile("productos.txt", 'utf-8');
+                    res(objs);
+                }, 3000)
+            })*/
+
+            /*
+            const objs = await fs.readFile("productos.txt", 'utf-8', function(err, data){
+                if(err) console.log('Error en read ' + err);
+
+                console.log('dentrooooo')
+                console.log(data);
+                //objs = JSON.parse(data);
+                return JSON.parse(data);
+            });
+            */
+            /*
+            fs.promises.readFile("productos.txt".toString())
+                .then(contenido => {
+                    console.log('lectura exitosa de info.txt')
+                    const info = JSON.parse(contenido)
+                    console.log(info)
+                    return info;
+                }).catch(error => console.log(error))
+            */
+            console.log('despues de leer aaaaaa')
+            //console.log(objs);
+            //return JSON.parse(objs);
+        }catch(error){
+            console.log('Error en getAll ' + error);
+            return [];
+        }
+        //console.log(objs);
+        //return objs;
+
+        
+        /*open('productos.txt', 'r', (err, fd) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    console.error('myfile does not exist');
+                    return;
+                }
+                
+                throw err;
+            }
+            
+            try {
+                readMyData(fd);
+            } finally {
+                close(fd, (err) => {
+                    if (err) throw err;
+                });
+            }
+        });*/
+    }
+
+    async deleteById(id){
+        let info = [];
+        const objs = await this.getAll();
+        info = objs.filter((ob) => ob.id !== id);
+
+        await fs.promises.writeFile(this.path, JSON.stringify(info, null, 2))
+            .then(() => console.log('Objeto eliminado'))
+            .catch(error => console.log(error))
+    }
+
+    async deleteAll(){
+        await fs.promises.writeFile(this.path, '')
+            .then(() => console.log('Archivo vacio'))
+            .catch(error => console.log(error))
+    }
+}
+
+module.exports = Contenedor;
---
+++ server.js
@@ -0,0 +1,49 @@
+const Contenedor = require('./Contenedor.js');
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+const contenedor = new Contenedor("productos.txt");
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${server.address().port}`)
+})
+
+app.get('/productos', async (req, res) =>{
+    const data = await contenedor.getAll();
+    console.log(data);
+})
+
+/*
+app.get('/productos', (req, res) =>{
+    contenedor.getAll()
+        .then(res =>{
+            console.log(res);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+})
+*/
+/*
+app.get('/productos', async (req, res) => {
+    console.log('antes de la llamada getAll')
+    let objs = await contenedor.getAll();
+    console.log('termino de leer');
+    //res.render('home');
+    console.log('despues de la llamada getAll')
+    console.log(objs);
+    //res.send(objs)
+})
+*/
+
+app.get('/productosRandom', (req, res) => {
+    res.send(frase)
+})
+
+
+
+server.on('error', error => console.log(`Error en servidor ${error}`))
